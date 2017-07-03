10b827c42c4ea06c7d53c30d14f1789a56958a0a
Jesse Gielen <jessegielen@hotmail.com>
2017-07-03 23:29:44 +0200

    Missing License for OpenGLWindowSettings.cs

--- src/OpenGLWindowSettings.cs
+++ src/OpenGLWindowSettings.cs
@@ -1,4 +1,8 @@
-﻿namespace OpenWindow
+﻿// MIT License - Copyright (C) Jesse "Jjagg" Gielen
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace OpenWindow
 {
     public class OpenGLWindowSettings
     {
