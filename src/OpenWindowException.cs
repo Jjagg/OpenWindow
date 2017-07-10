95ebbe43b3dbcb2b93b40d8760ab0402fbfe42f7
Jesse Gielen <jessegielen@hotmail.com>
2017-07-10 02:07:39 +0200

    Custom exception type

---
+++ src/OpenWindowException.cs
@@ -0,0 +1,19 @@
+﻿// MIT License - Copyright (C) Jesse "Jjagg" Gielen
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+
+namespace OpenWindow
+{
+    public class OpenWindowException : Exception
+    {
+        public OpenWindowException(string message) : base(message)
+        {
+        }
+
+        public OpenWindowException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
