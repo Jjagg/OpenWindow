acdac3990b5532af2719e0ee53ec87e7546b926f
Jesse Gielen <jessegielen@hotmail.com>
2018-01-03 02:58:03 +0100

    Wayland: Detecting Displays

--- src/Backends/Wayland/WaylandWindowingService.cs
+++ src/Backends/Wayland/WaylandWindowingService.cs
@@ -3,20 +3,29 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace OpenWindow.Backends.Wayland
 {
     internal class WaylandWindowingService : WindowingService
     {
+        private List<Display> _displays;
         public override ReadOnlyCollection<Display> Displays { get; }
         public override Display PrimaryDisplay { get; }
+
+        private readonly Dictionary<IntPtr, Display> _pendingDisplays;
 
         private WlDisplay _wlDisplay;
         private WlRegistry _wlRegistry;
         private XdgWmBase _xdgWmBase;
         private WlCompositor _wlCompositor;
         private WlShm _wlShm;
+
+        internal WaylandWindowingService()
+        {
+            _pendingDisplays = new Dictionary<IntPtr, Display>();
+        }
 
         protected override void Initialize()
         {
@@ -25,6 +34,8 @@ namespace OpenWindow.Backends.Wayland
                 throw new OpenWindowException("Failed to connect to Wayland display.");
 
             _wlRegistry = _wlDisplay.GetRegistry();
+            if (_wlRegistry.IsNullPtr)
+                throw new OpenWindowException("Failed to connect to get Wayland registry.");
 
             _wlRegistry.Global = RegistryGlobal;
             _wlRegistry.GlobalRemove = RegistryGlobalRemove;
@@ -32,16 +43,18 @@ namespace OpenWindow.Backends.Wayland
 
             _wlDisplay.Roundtrip();
             _wlDisplay.Roundtrip();
-        }
 
-        private void RegistryGlobalRemove(IntPtr data, IntPtr iface, uint name)
-        {
+            _wlDisplay.Flush();
         }
 
         private void RegistryGlobal(IntPtr data, IntPtr registry, uint name, string iface, uint version)
         {
             switch (iface)
             {
+                case WlOutput.InterfaceName:
+                    var output = _wlRegistry.Bind<WlOutput>(name, WlOutput.Interface);
+                    AddDisplay(output);
+                    break;
                 case WlCompositor.InterfaceName:
                     _wlCompositor = _wlRegistry.Bind<WlCompositor>(name, WlCompositor.Interface);
                     break;
@@ -55,6 +68,58 @@ namespace OpenWindow.Backends.Wayland
                     _xdgWmBase = _wlRegistry.Bind<XdgWmBase>(name, XdgWmBase.Interface);
                     _xdgWmBase.Ping = PingHandler;
                     break;
+            }
+        }
+
+        private void RegistryGlobalRemove(IntPtr data, IntPtr iface, uint name)
+        {
+        }
+
+        private void AddDisplay(WlOutput output)
+        {
+            // keep track of the output and listen for configuration events
+            output.Geometry = OutputGeometryHandler;
+            output.Mode = OutputModeHandler;
+            output.Scale = OutputScaleHandler;
+            output.Done = OutputDoneHandler;
+            output.SetListener();
+            _pendingDisplays.Add(output.Pointer, new Display(output.Pointer));
+        }
+
+        private void OutputGeometryHandler(IntPtr data, IntPtr iface, int x, int y, int physicalWidth,
+            int physicalHeight, int subpixel, string make, string model, int transform)
+        {
+            if (_pendingDisplays.TryGetValue(iface, out var display))
+            {
+                display.Bounds = new Rectangle(x, y, physicalHeight, physicalHeight);
+                display.Name = make + " - " + model;
+            }
+            else
+            {
+                throw new OpenWindowException("Got an Output Geometry event for unknown output.");
+            }
+        }
+
+        private void OutputModeHandler(IntPtr data, IntPtr iface, uint flags, int width, int height, int refresh)
+        {
+            // TODO
+        }
+
+        private void OutputScaleHandler(IntPtr data, IntPtr iface, int factor)
+        {
+            // TODO
+        }
+
+        private void OutputDoneHandler(IntPtr data, IntPtr iface)
+        {
+            if (_pendingDisplays.TryGetValue(iface, out var display))
+            {
+                _displays.Add(display);
+                _pendingDisplays.Remove(iface);
+            }
+            else
+            {
+                throw new OpenWindowException("Got an Output Done event for unknown output.");
             }
         }
 
@@ -90,6 +155,7 @@ namespace OpenWindow.Backends.Wayland
 
         protected override void Dispose(bool disposing)
         {
+            // TODO check what actually needs explicit disposing
             _wlDisplay.Dispose();
             WaylandInterfaces.CleanUp();
             XdgShellInterfaces.CleanUp();
