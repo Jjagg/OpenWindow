ec0c64773e92b9d78dbfaa4001c4eed9546ab7e8
Jesse Gielen <jessegielen@hotmail.com>
2018-01-03 01:55:21 +0100

    Also target netstandard1.3 for convenience

--- src/Logger.cs
+++ src/Logger.cs
@@ -38,6 +38,10 @@ namespace OpenWindow
         {
             Messages = new List<Message>();
             _format = "[{0:T}] {1}: {2}";
+#if NETSTANDARD1_3
+            // default to stdout when available
+            _outputWriter = Console.Out;
+#endif
         }
 
         /// <summary>
