e68f9bc9c5d9ba6ce499cde3fc62725da56fe241
Jesse Gielen <jessegielen@hotmail.com>
2018-01-03 02:58:03 +0100

    Wayland: Detecting Displays

--- src/Display.cs
+++ src/Display.cs
@@ -19,22 +19,27 @@ namespace OpenWindow
         /// <summary>
         /// An identifier for the display.
         /// </summary>
-        public string Name { get; }
+        public string Name { get; internal set; }
 
         /// <summary>
         /// Desktop bounds of the display.
         /// </summary>
-        public Rectangle Bounds { get; }
+        public Rectangle Bounds { get; internal set; }
 
         /// <summary>
         /// Working area for the display
         /// </summary>
-        public Rectangle WorkingArea { get; }
+        public Rectangle WorkingArea { get; internal set; }
 
         /// <summary>
         /// <code>true</code> if this display is the primary display, <code>false</code> otherwise.
         /// </summary>
-        public bool IsPrimary { get; }
+        public bool IsPrimary { get; internal set; }
+
+        internal Display(IntPtr handle)
+        {
+            Handle = handle;
+        }
 
         internal Display(IntPtr handle, string name, Rectangle bounds, Rectangle workingArea, bool isPrimary)
         {
